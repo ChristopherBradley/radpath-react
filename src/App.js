300e9ab792267aed0f22fadbdb8a77cb9dc18245
Christopher Bradley <chris2bradley@gmail.com>
2024-01-30 23:04:03 +1100

    Finished node and edge drawing for react frontend

--- src/App.js
+++ src/App.js
@@ -4,13 +4,6 @@ import "./App.css";
   
 const NODE_SIZE = 10; 
 
-const findExistingNode = (x, y, nodes) => {
-    return nodes.findIndex(node => {
-        const [nx, ny] = node;
-        const distance = Math.sqrt((x - nx) ** 2 + (y - ny) ** 2);
-        return distance < NODE_SIZE;
-    });
-}
 
 function App() { 
     const canvasRef = useRef(null); 
@@ -33,8 +26,8 @@ function App() {
         nodes.forEach(node => {
             ctx.beginPath();
             ctx.arc(node[0], node[1], NODE_SIZE, 0, 2 * Math.PI);
-            ctx.strokeStyle = lineColor; // Set the stroke color
-            ctx.lineWidth = 1; // Set the line width
+            ctx.strokeStyle = lineColor;
+            ctx.lineWidth = 1;
             ctx.stroke();
             ctx.closePath();
           });
@@ -44,13 +37,25 @@ function App() {
             ctx.beginPath();
             ctx.moveTo(lastPress[0], lastPress[1]);
             ctx.lineTo(mousePosition[0], mousePosition[1]);
-            ctx.strokeStyle = lineColor; // Set the stroke color
-            ctx.lineWidth = 1; // Set the line width
+            ctx.strokeStyle = lineColor;
+            ctx.lineWidth = 1;
             ctx.stroke();
             ctx.closePath();
         }
 
-    }, [nodes, mousePosition]);  // useEffect runs whenever these variables get changed
+        // Draw the edges
+        edges.forEach(edge => {
+            ctx.beginPath();
+            ctx.moveTo(edge[0][0], edge[0][1]);
+            ctx.lineTo(edge[1][0], edge[1][1]);
+            ctx.strokeStyle = lineColor;
+            ctx.lineWidth = 1;
+            ctx.stroke();
+            ctx.closePath();
+        });
+
+    }, [nodes, mousePosition, edges]);  // useEffect runs whenever these variables get changed
+    
   
     const mouseDown = (e) => { 
         const x = e.nativeEvent.offsetX;
@@ -86,7 +91,8 @@ function App() {
         const node1 = lastPress;
         const existingNodeIndex = findExistingNode(x, y, nodes);
         const node2 = (existingNodeIndex !== -1) ? nodes[existingNodeIndex] : [x, y];
-
+        const existingEdgeIndex = findExistingEdge(node1, node2, edges);
+        
         // Clear the last press and mouse position to stop drawing a new edge
         setLastPress(null);
         setMousePosition(null);
@@ -97,28 +103,26 @@ function App() {
             if (newNode){
                 return;
             }
-            // Ignoring a node connected by an edge (must delete the edges first)
-            console.log("bla")
-
-            // Removing an existing node
-            nodes.splice(existingNodeIndex, 1);
-            return;
+            // Removing an existing node that isn't connected by an edge
+            if (!isNodeInEdges(node2, edges)){
+                nodes.splice(existingNodeIndex, 1);
+                return;
+            }
         }
 
-        // Placing a new node connected by an edge
+        // Placing a new node about to be connected by an edge
         if (existingNodeIndex === -1){
             setNodes([...nodes, node2]);
         } 
 
-        // TODO: Placing a new edge 
-        const edge = [node1,node2]
-        const reverse_edge = [node2, node1]
+        // Placing a new edge 
+        if (existingEdgeIndex === -1){
+            setEdges([...edges, [node1, node2]]);
 
-        // TODO: Removing an existing edge 
-
-
-
-        console.log("nodes", nodes)
+        // Removing an existing edge
+        } else {
+            edges.splice(existingEdgeIndex, 1);
+        }
     }; 
     
     return ( 
@@ -139,5 +143,28 @@ function App() {
     ); 
 
 }
+
+const findExistingNode = (x, y, nodes) => {
+    return nodes.findIndex(node => {
+        const [nx, ny] = node;
+        const distance = Math.sqrt((x - nx) ** 2 + (y - ny) ** 2);
+        return distance < NODE_SIZE;
+    });
+}
+
+const findExistingEdge = (node1, node2, edges) => {
+    return edges.findIndex(edge => {
+        return (
+            (edge[0][0] == node1[0] && edge[0][1] == node1[1] &&
+             edge[1][0] == node2[0] && edge[1][1] == node2[1]) ||
+            (edge[0][0] == node2[0] && edge[0][1] == node2[1] &&
+             edge[1][0] == node1[0] && edge[1][1] == node1[1])
+        );
+    });
+};
+
+const isNodeInEdges = (node, edges) => {
+    return edges.some(edge => edge.some(edgeNode => edgeNode[0] == node[0] && edgeNode[1] == node[1]));
+};
   
 export default App;
